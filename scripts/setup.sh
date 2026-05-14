b6a96cfa47043abb6c9afb26c6dc47418bca72d5
rangetik <dobroiranget@gmail.com>
2026-05-14 12:23:55 +0300

    add main setup

---
+++ scripts/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+echo "Початок розгортання"
+
+chmod +x scripts/*.sh
+
+./scripts/packages.sh
+
+./scripts/users.sh
+
+./scripts/database.sh
+
+sudo mkdir -p /opt/mywebapp
+sudo mkdir -p /etc/mywebapp
+sudo cp -r src /opt/mywebapp/
+sudo cp package.json /opt/mywebapp/
+sudo cp config/config.json /etc/mywebapp/config.json
+
+cd /opt/mywebapp
+sudo npm install --only=production
+sudo chown -R app:app /opt/mywebapp 
+cd -
+
+sudo cp config/systemd/mywebapp.socket /etc/systemd/system/
+sudo cp config/systemd/mywebapp.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable --now mywebapp.socket
+
+sudo cp config/nginx.conf /etc/nginx/sites-available/mywebapp
+sudo ln -sf /etc/nginx/sites-available/mywebapp /etc/nginx/sites-enabled/
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo systemctl restart nginx [cite: 77]
+
+echo "Розгоратання завершено успішно"
