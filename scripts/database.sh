0946f6a6c45086400194bf1a47a242b0ef7b3db1
rangetik <dobroiranget@gmail.com>
2026-05-14 12:16:36 +0300

    add database setup, systemd socket activation and nginx config

---
+++ scripts/database.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DB_NAME="mywebapp"
+DB_USER="app"
+DB_PASS="password123"
+
+sudo -u postgres psql -c "CREATE DATABASE $DB_NAME;"
+sudo -u postgres psql -c "CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASS';"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
+
+sudo -u postgres psql -d $DB_NAME -c "GRANT ALL ON SCHEMA public TO $DB_USER;"
+
+echo "База даних $DB_NAME та користувач $DB_USER створені"
